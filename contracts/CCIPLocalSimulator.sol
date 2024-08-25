99b545759446b65ba243d8cdde56a7deb960203f
7gbice <7gbice@gmail.com>
2024-08-25 03:55:22 -0600

    Exploring CCIPSimulatorLocale

---
+++ contracts/CCIPLocalSimulator.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {CCIPLocalSimulator} from "@chainlink/local/src/ccip/CCIPLocalSimulator.sol";
+// import {CrossChainNameServiceLookup} from "./CrossChainNameServiceLookup.sol";
+// import {CrossChainNameServiceReceiver} from "./CrossChainNameServiceReceiver.sol";
+// import {CrossChainNameServiceRegister} from "./CrossChainNameServiceRegister.sol";
